979b482074916c0a960d92c340e1f54bb6cbeea9
Pascal Hennen <phennen@anynines.com>
2021-10-11 11:48:02 +0200

    [Maintenance] Configurable Port and Amount of Workers
    Added puma and created a config file for it.

--- src/test.rb
+++ src/test.rb
@@ -1,17 +1,11 @@
 require 'sinatra'
 
-# Disclaimer: Eine sehr simple Anwendung, man kann zu einem Key ein Value mappen, zB post -d "Name Vorname" und diese dann mit
-# get URI/Name abfragen, es wird dann das Value ausgegeben, get URI/all gibt alle Einträge aus
-
-# Das Programm ist wirklich sehr simpel gehalten, es finden keine tests statt und es muss aus dem . directory mit ruby src/test.rb ausgeführt werden
-# (das sind aber alles Sachen die man leicht fixen kann), die db Datei muss zB vorher existieren
-
 # API calls
 
 # get all saved data
 get '/all' do
   string = ''
-  get_data.each do |element|
+  data.each do |element|
     string += element
     string += "\n"
   end
@@ -20,7 +14,7 @@ end
 
 # get a value to a key from the db file
 get '/:key' do
-  get_value(params['key'].to_s)
+  value(params['key'].to_s)
 end
 
 # post a new entry for the db file
@@ -31,7 +25,7 @@ end
 # functions
 
 # get the whole data
-def get_data
+def data
   file = File.open('data/db')
   data = file.readlines.map(&:chomp)
   file.close
@@ -39,10 +33,10 @@ def get_data
 end
 
 # get a fitting value to a key
-def get_value(key)
-  data = get_data
-  data.each do |element|
-    return element.gsub(key + ' ', '') if element.include?(key)
+def value(key)
+  temp = data
+  temp.each do |element|
+    return element.gsub("#{key} ", '') if element.include?(key)
   end
   'key not present'
 end
