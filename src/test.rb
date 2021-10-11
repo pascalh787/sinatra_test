0092ce2a585a97ad83cc2fc69ef04d67806ee613
Pascal Hennen <phennen@anynines.com>
2021-10-11 11:23:08 +0200

    fix rubocop

--- src/test.rb
+++ src/test.rb
@@ -6,54 +6,50 @@ require 'sinatra'
 # Das Programm ist wirklich sehr simpel gehalten, es finden keine tests statt und es muss aus dem . directory mit ruby src/test.rb ausgeführt werden
 # (das sind aber alles Sachen die man leicht fixen kann), die db Datei muss zB vorher existieren
 
-
 # API calls
 
 # get all saved data
 get '/all' do
-    string = ''
-    for element in get_data
-        string += element
-        string += "\n"
-    end
-    string
+  string = ''
+  get_data.each do |element|
+    string += element
+    string += "\n"
+  end
+  string
 end
 
 # get a value to a key from the db file
 get '/:key' do
-    get_value "#{params['key']}"
+  get_value params['key'].to_s
 end
 
 # post a new entry for the db file
 post '/' do
-    post_data request.body.read
+  post_data request.body.read
 end
-
 
 # functions
 
 # get the whole data
-def get_data()
-    file = File.open 'data/db'
-    data = file.readlines.map(&:chomp)
-    file.close
-    return data
+def get_data
+  file = File.open 'data/db'
+  data = file.readlines.map(&:chomp)
+  file.close
+  data
 end
 
 # get a fitting value to a key
 def get_value(key)
-    data = get_data
-    for element in data
-        if element.include? key
-            return element.gsub key + " ", ""
-        end
-    end
-    return 'key not present'
+  data = get_data
+  data.each do |element|
+    return element.gsub key + ' ', '' if element.include? key
+  end
+  'key not present'
 end
 
 # put data into the db file
 def post_data(data)
-    file = File.open 'data/db', 'a'
-    file.puts data
-    file.close
+  file = File.open 'data/db', 'a'
+  file.puts data
+  file.close
 end
