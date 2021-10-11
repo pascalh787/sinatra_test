f9ce7e99c90367174068b93f07c09c89a41b6a50
Pascal Hennen <phennen@anynines.com>
2021-10-11 11:37:39 +0200

    bosh release
    WIP

---
+++ src/sinatra_app/sinatra_app.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+
+set :port, ENV['PUMA_PORT'] || 9292
+
+# API calls
+
+# get all saved data
+get '/all' do
+  string = ''
+  data.each do |element|
+    string += element
+    string += "\n"
+  end
+  string
+end
+
+# get a value to a key from the db file
+get '/:key' do
+  value(params['key'].to_s)
+end
+
+# post a new entry for the db file
+post '/' do
+  post_data(request.body.read)
+end
+
+# functions
+
+# get the whole data
+def data
+  file = File.open('db')
+  data = file.readlines.map(&:chomp)
+  file.close
+  data
+end
+
+# get a fitting value to a key
+def value(key)
+  temp = data
+  temp.each do |element|
+    return element.gsub("#{key} ", '') if element.include?(key)
+  end
+  'key not present'
+end
+
+# put data into the db file
+def post_data(data)
+  file = File.open('db', 'a')
+  file.puts(data)
+  file.close
+end
